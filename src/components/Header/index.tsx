a7f48d8083b506ab38750aa5b5ea0ebcb3058848
Jasminedns <jasminedanese@icloud.com>
2025-06-04 18:08:17 +0200

    working links, active classes, hover works

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,9 @@
-'use client';
-
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import DesktopNav from "@/components/DesktopNav";
 
 const Header = () => {
 
-    const pathName = usePathname();
-
     return (
-        <nav>
-            <Link href={"/"} className={pathName === "/" ? "border-2 bg-black text-white" : "bg-white"}>About the Game</Link>
-            <Link href={"/about-the-team"} className={pathName === "/about-the-team" ? "border-2 bg-black text-white" : "bg-white"}>ABout the Team</Link>
-            <Link href={"/project-info"} className={pathName === "/project-info" ? "border-2 bg-black text-white" : "bg-white"}>Project Info</Link>
-        </nav>
+        <DesktopNav />
     )
 }
 
