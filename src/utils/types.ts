e2ddb9da934d4598e4bcccb1b54256d209f3c839
Jasminedns <jasminedanese@icloud.com>
2025-06-05 19:56:28 +0200

    first array draft

---
+++ src/utils/types.ts
@@ -0,0 +1,153 @@
+type devTypes = {
+    name: string,
+    category: string,
+    role: string[]
+    image: string
+}
+
+const dev = [
+    {
+        name: "Zyrile Mae Martens",
+        category: "Art Team",
+        role: ["Prop Artist"],
+        image: "#"
+    },
+    {
+        name: "Julia Rosen",
+        category: "Art Team",
+        role: ["Environment Artist"],
+        image: "#"
+    },
+    {
+        name: "Saga Von Pfaler",
+        category: "Art Team",
+        role: ["Animator"],
+        image: "#"
+    },
+    {
+        name: "Mirjana Mrdjen",
+        category: "Art Team",
+        role: ["Animator"],
+        image: "#"
+    },
+    {
+        name: "Robin Eriksson",
+        category: "Art Team",
+        role: ["VFX Artist", "Art Director"],
+        image: "#"
+    },
+    {
+        name: "Riley Elwell",
+        category: "Game Design Team",
+        role: ["Technical Design"],
+        image: "#"
+    },
+    {
+        name: "Philip Jensen",
+        category: "Game Design Team",
+        role: ["Level Design"],
+        image: "#"
+    },
+    {
+        name: "Viktor Drakenberg",
+        category: "Game Design Team",
+        role: ["Systems", "Product Owner"],
+        image: "#"
+    },
+    {
+        name: "Yanlin Liu",
+        category: "Game Design Team",
+        role: ["Level Design", "UI"],
+        image: "#"
+    },
+    {
+        name: "Jesper Elmtoft",
+        category: "Programming Team",
+        role: ["System", "UI"],
+        image: "#"
+    },
+    {
+        name: "Dani Vilar",
+        category: "Programming Team",
+        role: ["Game Programmer"],
+        image: "#"
+    },
+    {
+        name: "Oliver Henriksson",
+        category: "Programming Team",
+        role: ["Game Programmer"],
+        image: "#"
+    },
+    {
+        name: "Samuel Antonsson",
+        category: "Programming Team",
+        role: ["Game Programmer"],
+        image: "#"
+    },
+    {
+        name: "Pauline Zander",
+        category: "Production & Release Team",
+        role: ["Game Producer"],
+        image: "#"
+    },
+    {
+        name: "Jessica Höken",
+        category: "Production & Release Team",
+        role: ["Project Manager"],
+        image: "#"
+    },
+    {
+        name: "Sakar Kanilany",
+        category: "Production & Release Team",
+        role: ["Release Manager"],
+        image: "#"
+    },
+    {
+        name: "Rafik Abdellaoui",
+        category: "Production & Release Team",
+        role: ["Release Manager"],
+        image: "#"
+    },
+    {
+        name: "Jasmine Danese",
+        category: "Front-end Team",
+        role: ["Front-end Developer"],
+        image: "#"
+    },
+    {
+        name: "Carl Åberg",
+        category: "Front-end Team",
+        role: ["Front-end Developer"],
+        image: "#"
+    },
+    {
+        name: "Priyadharshini Seetha Ram",
+        category: "Front-end Team",
+        role: ["Front-end Developer"],
+        image: "#"
+    },
+    {
+        name: "André Torabpour",
+        category: "Front-end Team",
+        role: ["Front-end Developer"],
+        image: "#"
+    },
+    {
+        name: "Mustafa Larhnimi",
+        category: "Front-end Team",
+        role: ["Front-end Developer"],
+        image: "#"
+    },
+    {
+        name: "Meaza Efrem",
+        category: "Front-end Team",
+        role: ["Front-end Developer"],
+        image: "#"
+    },
+    {
+        name: "Maedeh Zandieh",
+        category: "Front-end Team",
+        role: ["Front-end Developer"],
+        image: "#"
+    }
+]
