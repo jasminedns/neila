9492b43f4313e27a1419f627622abe1b762dd111
Jasminedns <jasminedanese@icloud.com>
2025-06-05 14:39:42 +0200

    and fixed header width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import "../../app/globals.css"
 const Header = () => {
 
     return (
-        <header className="w-screen flex flex-row p-4 justify-center gap-10 bg-black/80">
+        <header className="flex flex-row p-4 justify-center gap-10 bg-black/80">
             <DesktopNav />
         </header>
     )
