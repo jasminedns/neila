0ab0d7df45c8178401e55e03902222b41594de23
Jasminedns <jasminedanese@icloud.com>
2025-06-05 13:13:51 +0200

    added bg and button (draft)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import "@/app/globals.css";
+
 export default function Home() { 
   return (
     <>
-   <h1>NEILA</h1>
+      <h1>NEILA</h1>
+      <div className="hp__button--link">
+        <div className="p-2 border-2">
+          Check it out on itcho.io now!
+        </div>
+      </div>
     </>
     
   );
