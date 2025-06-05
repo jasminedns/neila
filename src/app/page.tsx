832405cdbc52362f0730b701f93345a033f20db4
Jasminedns <jasminedanese@icloud.com>
2025-06-05 14:30:13 +0200

    dowload button link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SiItchdotio } from "react-icons/si";
 import "@/app/globals.css";
 
 export default function Home() { 
@@ -5,11 +6,13 @@ export default function Home() {
     <>
       <h1>NEILA</h1>
       <div className="hp__button--link">
-        <div className="p-2 border-2">
-          Check it out on itcho.io now!
+        <div className="mx-auto self-center">
+          <div className="p-3 border-2 flex flex-row items-center justify-center gap-3 border-[#FFD700]/25 rounded-sm relative z-30">
+            <SiItchdotio size={32}/>
+            <a href="#" target="_blank" className="hover:underline">Check it out on itcho.io now!</a>
+          </div>
         </div>
       </div>
     </>
-    
   );
 }
