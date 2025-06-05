39ee81285a9982a990aadc6cca90226259096455
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-05 22:05:41 +0200

    creating about-the team intro page

--- src/components/TeamIntro/index.tsx
+++ src/components/TeamIntro/index.tsx
@@ -3,23 +3,23 @@ const TeamIntro = () => {
     return (
         <>
         
-        <div className="bg-[url('/images/Empty_background.png')] w-screen bg-cover relative  h-screen">
+        <div className="bg-[url('/images/Empty_background.png')] w-screen bg-cover relative h-screen">
            
                 <Image src="/images/crowd_silhouette.png" alt="crowd_silhouette" width={0}  height={0} sizes="100vw"  
-                className="w-full h-auto object-contain absolute bottom-16"></Image>
+                className="w-full h-auto object-contain absolute bottom-16 "></Image>
             
-                <Image  src="/images/Dvider_02.png" alt="crowd_silhouette" width={0} height={0} sizes="100vw" 
-                className="w-full h-auto object-contain absolute bottom-0">
+                <Image  src="/images/Dvider_02.png" alt="Gold_Divider" width={0} height={0} sizes="100vw" 
+                className="w-full h-auto object-contain absolute bottom-0 opacity-75">
                 </Image>    
-                <div className="absolute bottom-70 flex justify-center w-full shadow-inner ">
+                <div className="absolute bottom-90 sm:bottom-70 flex justify-center w-full shadow-inner ">
                 <Image src="/images/Meet_the_team.png" alt="meet_the_team" width={400} height={0} sizes="100vw"
-                className="w-[400px] h-auto shadow-inner"></Image>
+                className="w-[200px] sm:w-[200px] md:w-[400px] h-auto shadow-inner"></Image>
             </div>
-            <section className="text-white absolute bottom-18 flex justify-around m-8 w-screen">
-                <p>designers</p>
-                <p>Artists</p>
-                <p>Developers</p>
-                <p>Managers</p>
+            <section className="text-white absolute  bottom-5 sm:bottom-15 md:bottom-20 lg:bottom-40 flex justify-around min-w-full">
+                <p className="font-montserrat team_button text-xs md:text-xl p-2">Designers</p>
+                <p className="font-montserrat team_button text-xs md:text-xl p-2">Artists</p>
+                <p className="font-montserrat team_button text-xs md:text-xl p-2">Developers</p>
+                <p className="font-montserrat team_button text-xs md:text-xl p-2">Managers</p>
             </section>
         </div>
         
