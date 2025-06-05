40a648fce7ca9b94d75cc68239c67ca06e99a0ce
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-05 13:14:35 +0200

    using correct font and sizes

--- src/app/project-info/page.tsx
+++ src/app/project-info/page.tsx
@@ -4,32 +4,32 @@ const ProjectInfoPage = () => {
     return (
         <div className="min-h-screen bg-gray-800 p-6  text-white text-xl">
             <div className="flex flex-col md:justify-between md:flex-row m-6">
-            <h4 className="text-amber-300  m-3 text-2xl font-bold">The Project:</h4>
+            <h4 className="font-cinzel m-3 text-2xl font-bold">The Project:</h4>
             <div className="w-[200px]  self-center">
             <Image src="/futuregame_logo.png" alt="futuregame_logo" width={0}  height={0} sizes="100vw" 
              className="w-full h-auto">
             </Image>
             </div>
             </div>
-            <li className="m-6 mt-8">Project Neila is in early development.We welcome all feedback-especially
+            <li className="m-6 mt-8 font-montserrat text-2xl">Project Neila is in early development.We welcome all feedback-especially
              on the feel and clarity of the core gameplay.</li>
                       
-            <h5 className="m-6 mt-5">Our Design Goals:</h5>
+            <h5 className="m-6 mt-5 font-montserrat">Our Design Goals:</h5>
             
             <div className="m-6 mt-10"> 
-                <h6 className="font-bold mt-4 text-justify">Player Freedom</h6>
+                <h6 className="font-bold mt-4 text-justify font-montserrat">Player Freedom</h6>
                 <li className="mt-3 text-justify">We want to encourage player freedom by allowing players to to tackle 
                     obstacles and combat scenarios in a variety of ways,with many solutios to every problem.</li>
             </div> 
 
             <div className="m-6 mt-10">
-                <h6 className="mt-4 font-bold">Emergent Gameplay</h6>
+                <h6 className="mt-4 font-bold font-montserrat">Emergent Gameplay</h6>
                 <li className=" mt-3 text-justify">Game mechanics should be designed and implemented in a flexible way such that their interactions
                 lead to novel and interesting outcomes in gameplay.</li>
             </div>
 
             <div className="m-6 mt-10">
-                <h6 className="font-bold">Immersion</h6>
+                <h6 className="font-bold font-montserrat">Immersion</h6>
                     <li className="mt-3 text-justify">The game should tell its narrative primarily through the environment, without interruptiong the gameplay.
                     (show don't tell)</li>
             </div>
