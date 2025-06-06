7fa0b2bbb22b41d034cb77f991982a34c2f018d5
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-06 23:35:20 +0200

    fixing the width of screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import "../../app/globals.css"
 
 const Header = () => {
     return (
-        <header className="w-screen flex flex-row p-4 justify-center gap-10 bg-black/80">
+        <header className="w-full flex flex-row p-4 justify-center gap-10 bg-black/80">
             <div className="header_div header_div:hover">About the Game</div>
             <div className="header_div header_div:hover">About the Team</div>
             <div className="header_div header_div:hover">Project info</div>
--- src/components/TeamIntro/index.tsx
+++ src/components/TeamIntro/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 const TeamIntro = () => {
     return (
         <>
-        <div className="bg-[url('/images/Empty_background.png')] w-screen bg-cover relative h-screen">
+        <div className="bg-[url('/images/Empty_background.png')]  bg-cover relative h-screen">
            <Image src="/images/crowd_silhouette.png" alt="crowd_silhouette" width={0}  height={0} sizes="100vw"  
                 className="w-full h-auto object-contain absolute bottom-16 ">
             </Image>
