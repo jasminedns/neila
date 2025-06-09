75cb63e8f28adf7a00786817f4d47e0b88565a7b
Maedeh86 <Maedeh.zandieh@gmail.com>
2025-06-09 15:21:29 +0200

    color changed

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,14 +5,14 @@ import {FaVk, FaFacebookF, FaYoutube, FaItchIo} from  "react-icons/fa";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-[#3B2F2F] text-[#C1A35C] py-6 px-4 text-center">
+        <footer className="bg-[#3B2F2F] text-[#FFFFFF] py-6 px-4 text-center">
       <div className="flex justify-center gap-6 mb-4 text-xl">
-        <a href="#" aria-label="VK" className="hover:text-[#FF8C42]"><FaVk /></a>
-        <a href="#" aria-label="Facebook" className="hover:text-[#FF8C42]"><FaFacebookF /></a>
-        <a href="#" aria-label="YouTube" className="hover:text-[#FF8C42]"><FaYoutube /></a>
-        <a href="#" aria-label="Itch.io" className="hover:text-[#FF8C42]"><FaItchIo /></a>
+        <a href="#" aria-label="VK" className="hover:text-[#FFFFFF]"><FaVk /></a>
+        <a href="#" aria-label="Facebook" className="hover:text-[#FFFFFF]"><FaFacebookF /></a>
+        <a href="#" aria-label="YouTube" className="hover:text-[#FFFFFF]"><FaYoutube /></a>
+        <a href="#" aria-label="Itch.io" className="hover:text-[#FFFFFF]"><FaItchIo /></a>
       </div>
-      <p className="text-sm max-w-xl mx-auto text-[#A9A9A9]">
+      <p className="text-sm max-w-xl mx-auto text-[#FFFFFF]">
         © 2023, CounterCurrent Games. All rights to the franchise belong to the original owner. 
         This project was created as part of our education at Futuregames and is not intended for 
         commercial use. We do not claim any ownership of the original product.
