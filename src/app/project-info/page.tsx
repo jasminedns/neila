0e23edbc36cf6f453098a712c28371e7e2a0ac2d
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-09 14:26:50 +0200

    removing the smoke image

--- src/app/project-info/page.tsx
+++ src/app/project-info/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import ProjectInfoIntroDiv from "@/components/ProjectInfoIntro";
-import StudentProjectDiv from "@/components/StudentProjectDiv";
 
 const ProjectInfoPage = () => {
     return (
         
         <div className="min-h-screen bg-[url('/images/Background_dvider.png')] m-o bg-cover p-0  text-white text-xl">
            <ProjectInfoIntroDiv />
-           <StudentProjectDiv />
         </div>
      
     );
