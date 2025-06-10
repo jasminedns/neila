bdaf484d2bfe5b63ea83a065d2658dc2a134cac0
Jasminedns <jasminedanese@icloud.com>
2025-06-10 15:06:59 +0200

    fixed responsivness

--- src/components/DevCard/index.tsx
+++ src/components/DevCard/index.tsx
@@ -5,7 +5,7 @@ import { devTypes } from "@/utils/types"
 const DevCard = ({name, category, role, image }:devTypes) => {
     return (
         <> 
-            <div className="flex flex-col m-3">
+            <div className="flex flex-col m-[48px] font-montserrat sm: m-[24px]">
                 <div className="border-3 border-[#FFD700] w-[100%] devCard__image--container">
                     <img 
                         src={image} 
@@ -14,10 +14,18 @@ const DevCard = ({name, category, role, image }:devTypes) => {
                         onError={(e) => { (e.currentTarget as HTMLImageElement).src = '/devs/unknown.jpg'; }} 
                     />
                 </div>
-                <div className="flex flex-col items-center text-xl">
-                    <h2 className="text-center">{name.split(" ")[0]} {name.split(" ")[1]}</h2>
-                    <h2>{name.split(" ")[2]}</h2>
-                </div>
+                {name && name === "Priyadharshini Seetha Ram" 
+                    ? 
+                        <div className="flex flex-col flex-wrap items-center text-xl mt-2">
+                            <h2>{name.split(" ")[0]}</h2>
+                            <h2>{name.split(" ")[1]} {name.split(" ")[2]}</h2>
+                        </div>
+                    : 
+                        <div className="flex flex-col flex-wrap items-center text-xl mt-2">
+                            <h2>{name.split(" ")[0]} {name.split(" ")[1]}</h2>
+                            <h2>{name.split(" ")[2]}</h2>
+                        </div>
+                }
             </div>
         </>
     )
