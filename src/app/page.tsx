303465e145c14ac5ac915a58204ae71ad461635e
Mustafa Larhnimi <mustafa.larhnimi@gmail.com>
2025-06-10 13:36:02 +0200

    updated trailer section with responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import TrailerSection from "@/components/TrailerSection";
 
 export default function Home() { 
   return (
     <>
       <h1>NEILA</h1>
-      <TrailerSection />
     </>
     
   );
