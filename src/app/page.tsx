25db31cdf9be2f943af1364a1e09ca0a947512f5
andre84o <andre.torabpour@gmail.com>
2025-06-12 15:31:44 +0200

    -change cods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SiItchdotio } from "react-icons/si";
 import "@/app/globals.css";
+import StoryElement from "@/components/StoryElement";
 
 
 export default function Home() { 
@@ -14,6 +15,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <StoryElement/>
     </>
   );
 }
--- src/components/StoryElement/index.tsx
+++ src/components/StoryElement/index.tsx
@@ -2,7 +2,7 @@ const StoryElement = () => {
     return (
         <>
         <div>
-            <h3 className="font-montserrat z-10">
+            <h3 className="font-montserrat z-10 text-[#FFD700] text-xl font-bold">
                 The Story :
             </h3>
         </div>
