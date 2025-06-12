3a15405a7dd3e4af8de430f9fc80f73b1df0df1f
Maedeh86 <Maedeh.zandieh@gmail.com>
2025-06-13 00:28:26 +0200

    Implement responsive homepage  background - Ticket-07/HP-Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { SiItchdotio } from "react-icons/si";
 import "@/app/globals.css";
+import HomePageLogo from "@/components/HomePageLogo";
 
 export default function Home() { 
   return (
     <>
+      <HomePageLogo />
       <h1>NEILA</h1>
       <div className="hp__button--link">
         <div className="mx-auto self-center">
           <div className="p-3 border-2 flex flex-row items-center justify-center gap-3 border-[#FFD700]/25 rounded-sm relative z-30">
-            <SiItchdotio size={32}/>
+            <SiItchdotio size={32} />
             <a href="#" target="_blank" className="hover:underline">Check it out on itcho.io now!</a>
           </div>
         </div>
--- src/components/HomePageLogo/index.tsx
+++ src/components/HomePageLogo/index.tsx
@@ -1,4 +1,3 @@
-
 import Image from 'next/image';
 import React from 'react';
 
@@ -6,23 +5,13 @@ const HomePageLogo = () => {
   return (
     <div className="relative w-full h-screen">
       <Image
-        src="/Neila_full_background.png" 
+        src="/images/Neila_full_background.png"
         alt="Homepage Background"
-        layout="fill"
-        objectFit="cover"
+        fill
+        className="object-cover"
         priority
       />
-
       
-      <div className="absolute inset-0 flex items-center justify-center">
-        <Image
-          src="/Background_logo.png"
-          alt="Steam Wrecked Logo"
-          width={180}
-          height={180}
-          className="object-contain"
-        />
-      </div>
     </div>
   );
 };
