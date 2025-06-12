3e848e94bef19c15d5a3004d180857773d1138b3
andre84o <andre.torabpour@gmail.com>
2025-06-12 15:18:42 +0200

    add cods

--- src/components/StoryElement/index.tsx
+++ src/components/StoryElement/index.tsx
@@ -1,6 +1,12 @@
 const StoryElement = () => {
     return (
-        <></>
+        <>
+        <div>
+            <h3 className="font-montserrat z-10">
+                The Story :
+            </h3>
+        </div>
+        </>
     )
 }
 
