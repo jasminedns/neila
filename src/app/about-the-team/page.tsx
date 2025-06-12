21ce18721c3a4aba7c4234af1d7a5649bdc9c8d3
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-12 23:26:44 +0200

    adding team page paragraph

--- src/app/about-the-team/page.tsx
+++ src/app/about-the-team/page.tsx
@@ -1,11 +1,13 @@
 import TeamIntro from "@/components/TeamIntro";
 import DevCategory from "@/components/DevCategory";
+import Team_Page_pragraph from "@/components/Team_Page_Paragraph";
 
 const AboutTheTeam = () => {
     return (
         <>
             <TeamIntro />
             <DevCategory />
+            <Team_Page_pragraph />
         </>
     )
 }
