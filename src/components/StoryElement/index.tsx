93257ccaf5dd08124d1ef6f8981ff8e1278f8f9d
andre84o <andre.torabpour@gmail.com>
2025-06-12 16:47:52 +0200

    add ned cod ans text

--- src/components/StoryElement/index.tsx
+++ src/components/StoryElement/index.tsx
@@ -2,9 +2,21 @@ const StoryElement = () => {
     return (
         <>
         <div>
-            <h3 className="font-montserrat z-10 text-[#FFD700] text-xl font-bold">
+            <h3 className="font-cinzel z-10 text-gold text-4xl font-bold text-right mr-8">
                 The Story :
             </h3>
+            <div className="flex justify-end w-full min-h-[150px]">
+            <div className="flex items-center justify-center mr-8 w-full max-w-4xl bg-[#FFD700]/10 border border-[#FFD700]/20 rounded-sm p-4 min-h-[150px]">
+            <p className="text-center leading-snug text-white ">
+            Captured on an alien planet and infused with telekinetic powers and elemental abilities, you must creatively
+            leverage your enhanced arm to escape a chaotic research facility filled with dangerously modified creatures. 
+            Experiment with manipulating the environment and applying elemental effects to objects to overcome
+            </p>
+            <p className="text-white text-center font-semibold mt-4">
+                obstacles and enemies to survive.
+            </p>
+            </div>
+        </div>
         </div>
         </>
     )
