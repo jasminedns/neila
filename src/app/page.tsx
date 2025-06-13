a43d05666f4cf02212279afb0988bcb0faf8412b
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-13 16:16:13 +0200

    changing bg in homepage button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <h1>NEILA</h1>
       <div className="hp__button--link">
         <div className="mx-auto self-center">
-          <div className="p-3 border-2 flex flex-row items-center justify-center gap-3 border-[#FFD700]/25 rounded-sm relative z-30">
+          <div className="p-3 border-2 flex flex-row items-center justify-center  bg-[#FFD700]/5 shadow-xl team_button gap-3 border-[#FFD700] rounded-sm relative z-30">
             <SiItchdotio size={32} fill="white"/>
             <a href="#" target="_blank" className="hover:underline font-montserrat">Check it out on itcho.io now!</a>
           </div>
