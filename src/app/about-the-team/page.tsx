18ff6df4b2df7aefc3dbe10b1be5ef0014cb4a64
Maedeh86 <Maedeh.zandieh@gmail.com>
2025-06-13 15:42:50 +0200

    Add team filter logic to About the Team section

--- src/app/about-the-team/page.tsx
+++ src/app/about-the-team/page.tsx
@@ -1,6 +1,11 @@
+
+import OnClickTeamMenu from "@/components/onclick-team-menu";
 const AboutTheTeam = () => {
     return (
-        <h2>About the team</h2>
+        <>
+            <h2>About the team</h2>
+            <OnClickTeamMenu />
+        </>
     )
 }
 
