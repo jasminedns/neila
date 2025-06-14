e29508fa98e3374abd96055bb5edcd54ee7a1a92
Jasminedns <jasminedanese@icloud.com>
2025-06-14 16:44:41 +0200

    fixed some sizes

--- src/components/CoverImage/index.tsx
+++ src/components/CoverImage/index.tsx
@@ -2,10 +2,9 @@ import Image from "next/image"
 
 const CoverImage = () => {
     return (
-        <div className="absolut max-w-[300px] sm:max-w-[150px] md:max-w-[250px]" >
-        <Image src="/images/Game_cover.png" alt="cover" width={300} height={400}/>
+        <div className="hidden lg:block lg:absolute lg:max-w-[250px]" >
+            <Image src="/images/Game_cover.png" alt="cover" width={300} height={400}/>
         </div>
-        
     )
 }
 
--- src/components/ImageCarousel/index.tsx
+++ src/components/ImageCarousel/index.tsx
@@ -4,14 +4,12 @@ import { IoChevronBack, IoChevronForward } from 'react-icons/io5';
 import Image from "next/image";
 
 const images = [
-  '/images/test.jpg',
-  '/images/Dvider_01.png',
-  '/images/test.jpg',
-  '/images/Dvider_01.png',
-  '/images/test.jpg',
-  '/images/Dvider_01.png',
-  '/images/test.jpg',
-  '/images/Dvider_01.png',
+  '/images/first-slide.png',
+  '/images/second-slide.png',
+  '/images/third-slide.png',
+  '/images/first-slide.png',
+  '/images/second-slide.png',
+  '/images/third-slide.png'
 ];
 
 const ImageCarousel = () => {
@@ -34,29 +32,24 @@ const ImageCarousel = () => {
   return (
     <>
       {/* Mobile Buttons */}
-      <div className="flex justify-center gap-6 mb-4 md:hidden">
-        <button
-          onClick={prev}
-          className="hover:scale-125 transition-transform min-md:hidden">
-          <IoChevronBack size={42} color="gold" />
-        </button>
-        <button
-          onClick={next}
-          className="hover:scale-125 transition-transform md:hidden">
-          <IoChevronForward size={42} color="gold" />
-        </button>
+      <div className="flex justify-center gap-6 mb-4 mt-[270px] md:hidden">
+        {startIndex !== 0 && 
+          <IoChevronBack size={42} color="gold" onClick={prev} className="hover:scale-125 transition-transform min-md:hidden"/>
+        }
+        {
+          startIndex !== 3 &&
+          <IoChevronForward size={42} color="gold" onClick={next} className="hover:scale-125 transition-transform min-md:hidden"/>
+        }
       </div>
 
       {/* Desktop Layout */}
-      <div className="relative w-full flex flex-col sm:flex-row justify-center items-center gap-4 mt-10 mb-20 px-4 overflow-x-hidden">
+      <div className="relative w-full flex flex-col sm:flex-row justify-center items-center gap-4 mt-10 mb-20 px-4 overflow-x-hidden md:mt-[300px] lg:mt-[400px]">
         <div className="flex flex-col md:flex-row gap-6 w-full justify-center items-center overflow-x-hidden px-4">
 
           {/* Desktop Left Button */}
-          <button
-            onClick={prev}
-            className="relative hover:scale-125 transition-transform ml-0 md:block hidden">
-            <IoChevronBack size={42} color="gold" />
-          </button>
+          {startIndex !== 0 && 
+            <IoChevronBack size={42} color="gold" onClick={prev} className="hover:scale-125 transition-transform hidden md:block"/>
+          }
 
           {/* Images */}
           {visibleImages.map((src, index) => (
@@ -73,12 +66,10 @@ const ImageCarousel = () => {
           ))}
 
           {/* Desktop Right Button */}
-          <button
-            onClick={next}
-            className="relative hover:scale-125 transition-transform mr-10 md:block hidden">
-            <IoChevronForward size={42} color="gold" />
-          </button>
-
+          {
+            startIndex !== 3 &&
+            <IoChevronForward size={42} color="gold" onClick={next} className="hover:scale-125 transition-transform hidden md:block"/>
+          }
         </div>
       </div>
     </>
