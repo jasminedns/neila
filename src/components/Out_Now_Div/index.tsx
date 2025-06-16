6f7ed4c72c866dd1518129c63895674e7d639422
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-06-16 14:12:59 +0200

    align image width

--- src/components/Out_Now_Div/index.tsx
+++ src/components/Out_Now_Div/index.tsx
@@ -3,12 +3,12 @@ import Image from "next/image"
 const Out_Now_Div = () => {
     return (
         <>
-        <div className="flex flex-col items-center text-center align-center py-6">
-            <p className="font-montserrat text-[14px] md:text-[20px] mx-12">Out Now</p>
+        <div className="flex flex-col items-center text-center align-center pt-6">
+            <p className="font-montserrat text-[14px] md:text-[20px] mx-12 md:mx-0">Out Now</p>
         
-                <a href = "https://futuregames.itch.io/project-neila">
+                <a href = "https://futuregames.itch.io/project-neila" className="flex justify-center">
                     <Image src="/out_now_no_arrows.png" alt="out_now" width={0} height={0} sizes="100vw"
-                        className="w-[75%]  h-auto md:mx-12"></Image>
+                        className=" w-[100%]  md:w-[75%] lg:w-[50%] h-auto md:mx-12"></Image>
                             </a>
                             </div> 
                    
