b476df1a409987140c87edc6febe81c7a438cf87
Maedeh86 <Maedeh.zandieh@gmail.com>
2025-06-16 17:54:56 +0200

    Ticket-36/hp-button – updated itchio link, removed NEILA h1, and adjusted font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ import Mechanics_Element from "@/components/Mechanics_Element";
 export default function Home() { 
   return (
     <>
-      <h1>NEILA</h1>
-
       <div className="hp__button--link">
         <HomePageLogo />
       </div> 
@@ -18,7 +16,13 @@ export default function Home() {
         <div className="mx-auto self-center">
           <div className="p-3 border-2 flex flex-row items-center justify-center bg-[#FFD700]/5 shadow-xl team_button gap-3 border-[#FFD700] rounded-sm relative z-30">
             <SiItchdotio size={32} fill="white"/>
-            <a href="#" target="_blank" className="hover:underline font-montserrat">Check it out on itcho.io now!</a>
+           <a 
+  href="https://futuregames.itch.io/project-neila" 
+  target="_blank" 
+  className="hover:underline font-montserrat text-base md:text-[20px] lg:text-[20px]"
+>
+  Check it out on itcho.io now!
+</a>
           </div>
         </div>
       </div>
