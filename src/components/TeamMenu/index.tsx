e53d58ab44437ad560eaa8175c5cacf6d433cad4
andre84o <andre.torabpour@gmail.com>
2025-06-17 14:45:06 +0200

    fixed border colore

--- src/components/TeamMenu/index.tsx
+++ src/components/TeamMenu/index.tsx
@@ -14,7 +14,6 @@ const TeamMenu = ({ category }: TeamMenuTypes) => {
 
   return (
     <>
-      {/* Desktop */}
       <div className="hidden md:flex justify-center gap-6 py-6 absolute bottom-5 left-1/2 transform -translate-x-1/2 mt-160 z-10">
         {categories.map(cat => (
           <p
@@ -28,15 +27,11 @@ const TeamMenu = ({ category }: TeamMenuTypes) => {
       </div>
 
       {/* Mobile */}
-<div className="md:hidden relative z-50">
+  <div className="md:hidden relative z-50">
   <div className="flex flex-row-reverse items-center justify-end transform translate-y-[75vh] gap-2 w-full px-4">
     
-    {/* Toggle button – behåller pilen till höger */}
-    <div className="absolute ml-12 right-2">
-    <button
-      onClick={() => setOpen(!open)}
-      className="p-2 rounded-full bg-black shadow-lg"
-    >
+    <div className="absolute ml-12 right-0 top-1/2 transform -translate-y-1/2">
+    <button onClick={() => setOpen(!open)} className="p-2 rounded-full bg-black shadow-lg">
       {open ? (
         <ChevronRight className="text-yellow-400 w-6 h-6 animate-pulse" />
       ) : (
@@ -45,23 +40,19 @@ const TeamMenu = ({ category }: TeamMenuTypes) => {
     </button>
     </div>
 
-    {/* Animated responsive menu */}
     <div
       className={`
         transition-transform duration-500 ease-in-out
-        ${open ? 'translate-x-0' : '-translate-x-full'}
-          rounded-md px-3 py-3 w-[80vw] ml-4
-      `}
-    >
+        ${open ? 'translate-x-0' : '-translate-x-[100vw]'}
+          rounded-md px-3 py-3 w-[80vw] ml-4`}>
+
       <div className="flex flex-wrap justify-center gap-3 -mt-3 sm:mt-0 mx-auto w-auto ">
         {categories.map(cat => (
-          <p
-            key={cat}
+          <p key={cat}
             onClick={() => {
               category(cat);
-              setOpen(false);
             }}
-            className="sm:w-[120px] text-center text-white font-semibold text-[11px] sm:text-sm py-1 px-2 sm:py-2 sm:px-3 rounded-sm bg-[#00000078] hover:bg-yellow-500 hover:text-black transition"
+            className="sm:w-[120px] text-center text-white font-semibold text-[11px] sm:text-sm py-1 px-2 sm:py-2 sm:px-3 rounded-sm bg-[#00000078] hover:bg-yellow-500 hover:text-black border-[1px] border-[#ffd900af] transition"
           >
             {cat}
           </p>
