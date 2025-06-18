32fe46ac921834894445604537f61a332ff421fe
andre84o <andre.torabpour@gmail.com>
2025-06-18 21:57:14 +0200

    fix horizontal AG

--- src/components/TeamIntro/index.tsx
+++ src/components/TeamIntro/index.tsx
@@ -22,7 +22,7 @@ const TeamIntro = ({ clickedCat, chosenCategory }: TeamIntroTypes) => {
                 className="w-full h-auto object-contain absolute bottom-0 opacity-75">
             </Image>   
 
-            <div className="absolute bottom-70 md:bottom-75 lg:bottom-50 flex justify-center w-full shadow-inner ">
+            <div className="absolute bottom-70 md:bottom-75 lg:bottom-50 flex justify-center w-full shadow-inner team-image-position">
                 <Image src="/images/Meet_the_team.png" alt="meet_the_team" width={0} height={0} sizes="100vw"
                 className="w-[250px] sm:w-[250px] md:w-[350px] h-auto shadow-inner"></Image>
             </div>
