ea94257fcde86e0eb7c300d608f0c2c6956f456e
Mustafa Larhnimi <mustafa.larhnimi@gmail.com>
2025-06-18 13:38:52 +0200

    removed extra background on project page.tsx

--- src/app/project-info/page.tsx
+++ src/app/project-info/page.tsx
@@ -4,7 +4,7 @@ import ProjectInfoIntroDiv from "@/components/ProjectInfoIntro";
 const ProjectInfoPage = () => {
 
     return (        
-        <div className="min-h-screen bg-[url('/images/Background_dvider.png')] m-o bg-cover p-0  text-white text-xl">
+        <div>
            <ProjectInfoIntroDiv />
            <StudentProjectDiv />
         </div>
