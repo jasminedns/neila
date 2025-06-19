67b0627c7151fc392f30d3ec89218486340edca6
andre84o <andre.torabpour@gmail.com>
2025-06-19 18:45:49 +0200

    logo position fixed

--- src/components/HomePageLogo/index.tsx
+++ src/components/HomePageLogo/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePageLogo = () => {
   return (
-    <div className="relative w-full min-h-[70vh] md:min-h-[90vh] bg-[url('/images/Neila_full_background.png')] bg-cover bg-[47%]"></div>
+    <div className="relative w-full min-h-[70vh] md:min-h-[90vh] bg-[url('/images/Neila_full_background.png')] bg-cover bg-[47%] moveup"></div>
   );
 };
 
