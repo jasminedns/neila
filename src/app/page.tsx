1857e90636abb6fdc02d9a5e96b282b497837339
andre84o <andre.torabpour@gmail.com>
2025-06-19 12:08:48 +0200

    fixed vertical problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,18 +11,14 @@ export default function Home() {
         <HomePageLogo />
       </div> 
 
-      <div className="absolute bottom-0 md:top-[74%] lg:top-[74%] w-full min-h-[248px] flex justify-center bg-cover bg-[url('/border.png')] bg-[position:100%_5%] z-10 top-goldimage">
+      <div className="absolute bottom-0 md:top-[74%] lg:top-[74%] w-full min-h-[248px] flex justify-center bg-cover bg-[url('/border.png')] bg-[position:100%_5%] z-10 top-goldimage top-goldimage-portrait">
 
         <div className="mx-auto self-center">
-          <div className="p-3 border-2 flex flex-row items-center justify-center  bg-[#FFD700]/5 shadow-xl team_button gap-3 border-[#FFD700] rounded-sm relative z-30">
+          <div className="p-3 border-2 flex flex-row items-center sm:top[10px] justify-center  bg-[#FFD700]/5 shadow-xl team_button gap-3 border-[#FFD700] rounded-sm relative z-30">
             <SiItchdotio size={32} fill="white"/>
-           <a 
-  href="https://futuregames.itch.io/project-neila" 
-  target="_blank" 
-  className="hover:underline font-montserrat text-base md:text-[20px] lg:text-[20px]"
->
-  Check it out on itcho.io now!
-</a>
+           <a href="https://futuregames.itch.io/project-neila" target="_blank" className="hover:underline font-montserrat text-base md:text-[20px] lg:text-[20px]">
+              Check it out on itcho.io now!
+            </a>
           </div>
         </div>
       </div>
